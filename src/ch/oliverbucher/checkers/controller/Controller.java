7b5c4d2f600242817960917b149e912239b35350
Oliver Bucher <oliver.bucher@students.ffhs.ch>
2019-06-02 21:24:00 +0200

    first event handler

--- src/ch/oliverbucher/checkers/controller/Controller.java
+++ src/ch/oliverbucher/checkers/controller/Controller.java
@@ -1,4 +1,10 @@
 package ch.oliverbucher.checkers.controller;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void onClick(ActionEvent event) {
+        System.out.println("click is working!");
+    }
 }
