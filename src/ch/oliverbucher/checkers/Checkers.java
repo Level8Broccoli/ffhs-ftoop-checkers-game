0d725040ab4192cac5895659da34466915b86467
Oliver Bucher <oliver.bucher@students.ffhs.ch>
2019-06-05 15:25:20 +0200

    all power to the controller

--- src/ch/oliverbucher/checkers/Checkers.java
+++ src/ch/oliverbucher/checkers/Checkers.java
@@ -10,30 +10,13 @@ import ch.oliverbucher.checkers.view.javafx.JavaFXApplication;
 
 public class Checkers {
 
-    private static Board board;
-    private static Player[] players;
-
     public static void main(String[] args) {
 
-        initializeGame();
-
         JavaFXApplication javaFXApplication = new JavaFXApplication();
-        MainController controller = new MainController(javaFXApplication, board, players);
+        MainController controller = new MainController(javaFXApplication);
 
+        controller.initializeGame();
         controller.startInterface();
     }
 
-    private static void initializeGame() {
-
-        int boardWidth = Integer.parseInt(Config.getValue("BOARD_WIDTH"));
-        int boardHeight = Integer.parseInt(Config.getValue("BOARD_HEIGHT"));
-
-        board = new Board(boardWidth,boardHeight);
-
-        Player playerOne = new Player(PlayerType.HUMAN, PlayerColor.WHITE);
-        Player playerTwo = new Player(PlayerType.HUMAN, PlayerColor.BLACK);
-
-        players[0] = playerOne;
-        players[1] = playerTwo;
-    }
 }
