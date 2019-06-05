cb530476979556188ee32dfa7e25dbab00888652
Oliver Bucher <oliver.bucher@students.ffhs.ch>
2019-06-05 09:02:26 +0200

    check button via id

--- src/ch/oliverbucher/checkers/controller/Controller.java
+++ src/ch/oliverbucher/checkers/controller/Controller.java
@@ -1,10 +1,18 @@
 package ch.oliverbucher.checkers.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
 
 public class Controller {
 
+    @FXML
+    public ToggleButton btnHuman;
+    public ToggleButton btnComputer;
+
     public void onClick(ActionEvent event) {
         System.out.println("click is working!");
+        System.out.println("Human: " + btnHuman.isSelected());
+        System.out.println("Computer: " + btnComputer.isSelected());
     }
 }
