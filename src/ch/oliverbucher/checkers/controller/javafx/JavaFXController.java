1713cd9801d0c25c3b7a4dee2f0dc88cadc81bd6
Oliver Bucher <oliver.bucher@students.ffhs.ch>
2019-06-05 12:20:17 +0200

    scene switcher implemented

--- src/ch/oliverbucher/checkers/controller/javafx/JavaFXController.java
+++ src/ch/oliverbucher/checkers/controller/javafx/JavaFXController.java
@@ -1,15 +1,22 @@
-package ch.oliverbucher.checkers.controller;
+package ch.oliverbucher.checkers.controller.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class JavaFXController implements Initializable {
+
+    @FXML
+    private BorderPane container;
 
     @FXML
     private ToggleButton btnHuman;
@@ -20,6 +27,9 @@ public class JavaFXController implements Initializable {
     @FXML
     private Button btnStartGame;
 
+    private Scene gameScene;
+    private Scene launchScene;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btnStartGame.setOnAction(this::onClick);
@@ -29,5 +39,16 @@ public class JavaFXController implements Initializable {
         System.out.println("click is working!");
         System.out.println("Human: " + btnHuman.isSelected());
         System.out.println("Computer: " + btnComputer.isSelected());
+
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(gameScene);
+    }
+
+    public void setLaunchScene(Scene launchScene) {
+        this.launchScene = launchScene;
+    }
+
+    public void setGameScene(Scene gameScene) {
+        this.gameScene = gameScene;
     }
 }
--- src/ch/oliverbucher/checkers/view/javafx/JavaFXApplication.java
+++ src/ch/oliverbucher/checkers/view/javafx/JavaFXApplication.java
@@ -1,6 +1,6 @@
 package ch.oliverbucher.checkers.view.javafx;
 
-import ch.oliverbucher.checkers.controller.JavaFXController;
+import ch.oliverbucher.checkers.controller.javafx.JavaFXController;
 import ch.oliverbucher.checkers.view.ApplicationInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class JavaFXApplication extends Application implements ApplicationInterface {
-
+    
     @Override
     public void startApplication() {
         launch();
@@ -25,20 +25,33 @@ public class JavaFXApplication extends Application implements ApplicationInterfa
         String pathToConfig = "ch.oliverbucher.checkers.resources.";
         ResourceBundle resources = ResourceBundle.getBundle(pathToConfig + "config");
 
-        // Load fxml file and controller
-        URL fxmlURL = getClass().getResource("JavaFXApplication.fxml");
+        // Load controller
+        JavaFXController controller = new JavaFXController();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL, resources);
-        fxmlLoader.setController(new JavaFXController());
+        // Load stylesheet
+        String stylesheetFile = this.getClass().getResource("JavaFXApplicationStyles.css").toExternalForm();
 
-        Parent root = fxmlLoader.load();
+        // Launch Scene
+        URL launchFxmlURL = getClass().getResource("JavaFXApplicationLaunchScene.fxml");
+        FXMLLoader launchFxmlLoader = new FXMLLoader(launchFxmlURL, resources);
+        launchFxmlLoader.setController(controller);
+        Parent launchRoot = launchFxmlLoader.load();
+        launchRoot.getStylesheets().add(stylesheetFile);
+        Scene launchScene = new Scene(launchRoot);
 
-        // Load styles
-        root.getStylesheets().add(this.getClass().getResource("JavaFXApplicationStyles.css").toExternalForm());
+        // Game Scene
+        URL fxmlURLGame = getClass().getResource("JavaFXApplicationGameScene.fxml");
+        FXMLLoader fxmlLoaderGame = new FXMLLoader(fxmlURLGame, resources);
+        fxmlLoaderGame.setController(controller);
+        Parent gameRoot = fxmlLoaderGame.load();
+        gameRoot.getStylesheets().add(stylesheetFile);
+        Scene gameScene = new Scene(gameRoot);
+
+        controller.setGameScene(gameScene);
+        controller.setLaunchScene(launchScene);
 
         // Set scene and start application
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        stage.setScene(launchScene);
         stage.setTitle(resources.getString("GAME_TITLE"));
         stage.setWidth(Double.parseDouble(resources.getString("WINDOW_WIDTH")));
         stage.setHeight(Double.parseDouble(resources.getString("WINDOW_HEIGHT")));
